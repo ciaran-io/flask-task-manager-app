9a2fccab20300fca0657f7768456e6c8a3d865a4
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-03-12 13:04:14 +0000

    Add accordion & tooltip functionality.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav({ edge: 'right' });
+  $('.sidenav').sidenav({ edge: 'right' }); // open mobile nav from the right side of screen
+  $('.collapsible').collapsible(); // accordion
+  $('.tooltipped').tooltip(); // tooltip
 });
