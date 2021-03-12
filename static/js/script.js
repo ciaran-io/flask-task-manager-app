d8745d322bfa8142f3028b03a9f3812271518555
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-03-12 18:38:47 +0000

    Add category dropdown.

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
   $('.sidenav').sidenav({ edge: 'right' }); // open mobile nav from the right side of screen
   $('.collapsible').collapsible(); // accordion
   $('.tooltipped').tooltip(); // tooltip
+  $('select').formSelect(); // form select
+  $('.datepicker').datepicker({
+    format: 'dd mmmm, yyyy',
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: 'Select',
+    },
+  }); // datepicker
 });
